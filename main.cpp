98b624a9e30e5485e75811dbe4a0060ac49830ae
sora_suzuki <k022g0047@g.neec.ac.jp>
2026-02-17 19:57:57 +0900

    01_03完

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include<cstdio>
 #include<cstdlib>
 
-int main() {
-	printf("HelloWorld!\n");
-	system("pause");
+int main(int argc, char* argv[]) {
 
-	return 0;
+    //argc‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+    for (int i = 0; i < argc; i++) {
+        //•¶š—ñargv‚Ìi”Ô‚ğ•\¦
+        printf(argv[i]);
+
+        //‰üs
+        printf("\n");
+    }
+
+    system("pause");
+
+    return 0;
 }
