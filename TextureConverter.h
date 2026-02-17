ceb0eebccb47c6ecc3aeed6dda72169ecf79471d
sora_suzuki <k022g0047@g.neec.ac.jp>
2026-02-17 20:56:00 +0900

    01_05終わり

--- TextureConverter.h
+++ TextureConverter.h
@@ -13,4 +13,20 @@ private:
 
 	//ワイド文字列への変換
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+
+	//画像の情報
+	DirectX::TexMetadata metadata_;
+	//画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+
+	//ファイルパスの分解
+	void SeparateFilePath(const std::wstring& filePath);
+	//ディレクトリパス
+	std::wstring directoryPath_;
+	//ファイル名
+	std::wstring fileName_;
+	//ファイル拡張子
+	std::wstring fileExt_;
+	//テクスチャファイル書き出し
+	void SaveDDSTextureToFile();
 };
